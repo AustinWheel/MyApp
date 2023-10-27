45dd6bb8d9569e7def150d7c4cf5a1b1c88f693d
austinwheel <austinwheeler1112@gmail.com>
2023-10-27 02:09:55 -0400

    Initial Commit

---
+++ MyApp/MyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppApp.swift
+//  MyApp
+//
+//  Created by Austin Wheeler on 10/27/23.
+//
+
+import SwiftUI
+
+@main
+struct MyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

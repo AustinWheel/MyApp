e0f82f54ad517d124df28d00b509dd115f79434f
austinwheel <austinwheeler1112@gmail.com>
2023-10-27 03:36:51 -0400

    Add MapView, CircleView, and update ContentView

--- MyApp/ContentView.swift
+++ MyApp/ContentView.swift
@@ -10,12 +10,29 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            MapView()
+                .frame(height: 300)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack (alignment: .leading) {
+                Text("Turtle Rock!")
+                    .font(.title)
+                    .foregroundColor(.green)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+                Divider()
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eleifend dolor bibendum, dictum nisi a, feugiat lorem. Ut ultricies mi eget ex finibus, quis elementum dui ornare. Quisque lobortis sit amet ipsum quis hendrerit.")
+            }.padding()
+            Spacer()
         }
-        .padding()
     }
 }
 
